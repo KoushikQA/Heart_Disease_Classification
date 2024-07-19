d34563b32c02b9da448b0704b13fc2b008fd3df0
KOUSHIK PAL <111008211+KoushikQA@users.noreply.github.com>
2024-07-19 21:53:58 +0530

    Add files via upload

---
+++ trainMLModels.py
@@ -0,0 +1,170 @@
+import importPackages
+import exploratoryDataAnalysis
+
+# Creating dummy variables from categorical variables - 'cp', 'thal' and 'slope'
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+
+frames = [df, a, b, c]
+df = pd.concat(frames, axis = 1)
+df.head()
+
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+df.head()
+
+# Creating Model
+y = df.target.values
+x_data = df.drop(['target'], axis = 1)
+
+# Normalize Data
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values
+
+# Train-test split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
+
+#transpose matrices
+x_train = x_train.T
+y_train = y_train.T
+x_test = x_test.T
+y_test = y_test.T
+
+# Sklearn Logistic Regression
+accuracies = {}
+
+lr = LogisticRegression()
+lr.fit(x_train.T,y_train.T)
+acc = lr.score(x_test.T,y_test.T)*100
+
+accuracies['Logistic Regression'] = acc
+print("Test Accuracy {:.2f}%".format(acc))
+
+# K-Neighbors Classification
+
+# KNN Model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 2)  # n_neighbors means k
+knn.fit(x_train.T, y_train.T)
+prediction = knn.predict(x_test.T)
+
+print("{} NN Score: {:.2f}%".format(2, knn.score(x_test.T, y_test.T)*100))
+
+# try to find best k value
+scoreList = []
+for i in range(1,20):
+    knn2 = KNeighborsClassifier(n_neighbors = i)  # n_neighbors means k
+    knn2.fit(x_train.T, y_train.T)
+    scoreList.append(knn2.score(x_test.T, y_test.T))
+
+plt.plot(range(1,20), scoreList)
+plt.xticks(np.arange(1,20,1))
+plt.xlabel("K value")
+plt.ylabel("Score")
+plt.show()
+
+acc = max(scoreList)*100
+accuracies['KNN'] = acc
+print("Maximum KNN Score is {:.2f}%".format(acc))
+
+# Support Vector Machines
+from sklearn.svm import SVC
+svm = SVC(random_state = 1)
+svm.fit(x_train.T, y_train.T)
+
+acc = svm.score(x_test.T,y_test.T)*100
+accuracies['SVM'] = acc
+print("Test Accuracy of SVM Algorithm: {:.2f}%".format(acc))
+
+#Naive Bayes Algorithm
+from sklearn.naive_bayes import GaussianNB
+nb = GaussianNB()
+nb.fit(x_train.T, y_train.T)
+
+acc = nb.score(x_test.T,y_test.T)*100
+accuracies['Naive Bayes'] = acc
+print("Accuracy of Naive Bayes: {:.2f}%".format(acc))
+
+# Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier()
+dtc.fit(x_train.T, y_train.T)
+
+acc = dtc.score(x_test.T, y_test.T)*100
+accuracies['Decision Tree'] = acc
+print("Decision Tree Test Accuracy {:.2f}%".format(acc))
+
+# Random Forest Classification
+
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators = 1000, random_state = 1)
+rf.fit(x_train.T, y_train.T)
+
+acc = rf.score(x_test.T,y_test.T)*100
+accuracies['Random Forest'] = acc
+print("Random Forest Algorithm Accuracy Score : {:.2f}%".format(acc))
+
+# Comparing Models
+colors = ["purple", "green", "orange", "magenta","#CFC60E","#0FBBAE"]
+
+sns.set_style("whitegrid")
+plt.figure(figsize=(16,5))
+plt.yticks(np.arange(0,100,10))
+plt.ylabel("Accuracy %")
+plt.xlabel("Algorithms")
+sns.barplot(x=list(accuracies.keys()), y=list(accuracies.values()), palette=colors)
+plt.show()
+
+# Confusion Matrix
+
+# Predicted values
+y_head_lr = lr.predict(x_test.T)
+knn3 = KNeighborsClassifier(n_neighbors = 3)
+knn3.fit(x_train.T, y_train.T)
+y_head_knn = knn3.predict(x_test.T)
+y_head_svm = svm.predict(x_test.T)
+y_head_nb = nb.predict(x_test.T)
+y_head_dtc = dtc.predict(x_test.T)
+y_head_rf = rf.predict(x_test.T)
+
+from sklearn.metrics import confusion_matrix
+
+cm_lr = confusion_matrix(y_test,y_head_lr)
+cm_knn = confusion_matrix(y_test,y_head_knn)
+cm_svm = confusion_matrix(y_test,y_head_svm)
+cm_nb = confusion_matrix(y_test,y_head_nb)
+cm_dtc = confusion_matrix(y_test,y_head_dtc)
+cm_rf = confusion_matrix(y_test,y_head_rf)
+
+plt.figure(figsize=(24,12))
+
+plt.suptitle("Confusion Matrixes",fontsize=24)
+plt.subplots_adjust(wspace = 0.4, hspace= 0.4)
+
+plt.subplot(2,3,1)
+plt.title("Logistic Regression Confusion Matrix")
+sns.heatmap(cm_lr,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.subplot(2,3,2)
+plt.title("K Nearest Neighbors Confusion Matrix")
+sns.heatmap(cm_knn,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.subplot(2,3,3)
+plt.title("Support Vector Machine Confusion Matrix")
+sns.heatmap(cm_svm,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.subplot(2,3,4)
+plt.title("Naive Bayes Confusion Matrix")
+sns.heatmap(cm_nb,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.subplot(2,3,5)
+plt.title("Decision Tree Classifier Confusion Matrix")
+sns.heatmap(cm_dtc,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.subplot(2,3,6)
+plt.title("Random Forest Confusion Matrix")
+sns.heatmap(cm_rf,annot=True,cmap="Blues",fmt="d",cbar=False, annot_kws={"size": 24})
+
+plt.show()
+
+
+
